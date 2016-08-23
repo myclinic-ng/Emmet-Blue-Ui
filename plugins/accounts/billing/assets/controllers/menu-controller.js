3166eb7f5d4e2d15f1c8b8efb86d89989805d7d1
samshal <samueladeshina73@gmail.com>
2016-08-23 18:43:04 +0100

    Automated Push

--- plugins/accounts/billing/assets/controllers/menu-controller.js
+++ plugins/accounts/billing/assets/controllers/menu-controller.js
@@ -1,9 +1,14 @@
 angular.module("EmmetBlue")
 
 .controller("accountsBillingMenuController", function($scope, utils){
-	$scope.billingItems = [
-		"Pharmacceuticals", "Admission", "Patient", "Consultation", "Laboratory & Testing",
-		"Pharmaceuticals", "Admissiona", "Patienta", "Consultations", "Laboratory",
-		"Mortuary", "Accident One Timers"
-	];
+	var billingTypes = utils.serverRequest('/accounts-biller/billing-type/view', 'GET');
+	
+	billingTypes.then(function(response){
+		$scope.billingItems = [];
+		angular.forEach(response, function(value, key){
+			$scope.billingItems.push(value.BillingTypeName);
+		})
+	}, function(response){
+		utils.errorHandler(response);
+	});
 })
