658c28d1806a9cd873f52da976e3396bc9db5983
flashup4all <flashup4all@gmail.com>
2016-08-24 07:54:44 +0100

    mortuary ui

--- plugins/mortuary/assets/controllers/body-controllers.js
+++ plugins/mortuary/assets/controllers/body-controllers.js
@@ -51,10 +51,10 @@ angular.module("EmmetBlue")
 		DTColumnBuilder.newColumn('BodyID').withTitle('Body ID'),
 		DTColumnBuilder.newColumn('BodyTag').withTitle('Body Tag'),
 		DTColumnBuilder.newColumn('DateOfDeath').withTitle('Date Of Death'),
-		DTColumnBuilder.newColumn('TimeOfDeath').withTitle('Time Of Death'),
+		// DTColumnBuilder.newColumn('TimeOfDeath').withTitle('Time Of Death'),
 		DTColumnBuilder.newColumn('PlaceOfDeath').withTitle('Place Of Death'),
-		DTColumnBuilder.newColumn('BurialPlace').withTitle('Burial Place'),
-		DTColumnBuilder.newColumn('DeathPhysicianName').withTitle('Physician'),
+		// DTColumnBuilder.newColumn('BurialPlace').withTitle('Burial Place'),
+		DTColumnBuilder.newColumn('DeathPhysicianID').withTitle('Physician'),
 		DTColumnBuilder.newColumn(null).withTitle('Action').notSortable().renderWith(actionsMarkup)
 	];
 
