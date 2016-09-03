4cf2acd4be4892947eed94561ba4ed6544e84cf4
samshal <samueladeshina73@gmail.com>
2016-09-03 20:58:06 +0100

    Automated Push

--- plugins/accounts/billing/assets/controllers/manage-invoices/invoice-grid-directive.js
+++ plugins/accounts/billing/assets/controllers/manage-invoices/invoice-grid-directive.js
@@ -2,7 +2,21 @@ angular.module("EmmetBlue")
 
 .directive("ngInvoiceGrid", function(){
 	return {
-		restrict: "AE",
-		templateUrl: "plugins/accounts/billing/assets/includes/manage-invoices/invoice-grid.html"
+		restrict: "E",
+		scope:{
+			"gridData": "="
+		},
+		templateUrl: "plugins/accounts/billing/assets/includes/manage-invoices/invoice-grid.html",
+		controller: function($scope, utils){
+			var invoice = $scope.gridData;
+			$scope.invoice = {};
+			$scope.invoice.invoiceId = invoice.BillingTransactionMetaID;
+			$scope.invoice.invoiceNumber = invoice.BillingTransactionNumber;
+			$scope.invoice.invoiceAmount = invoice.BilledAmountTotal;
+			$scope.invoice.invoiceDate = invoice.DateCreatedDateOnly;
+			$scope.invoice.invoiceStatus = invoice.BillingTransactionStatus;
+			$scope.invoice.invoicePatientId = invoice.PatientID;
+			$scope.invoice.invoicePatientName = invoice.PatientName;
+		}
 	}
 });
