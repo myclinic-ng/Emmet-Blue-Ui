0b1cd074e9fec7062e8730a5ed8e082c1f6c6be8
samshal <samueladeshina73@gmail.com>
2016-09-20 20:05:56 +0100

    Automated Push

--- plugins/human-resources/assets/controllers/access-control-controller.js
+++ plugins/human-resources/assets/controllers/access-control-controller.js
@@ -2,7 +2,10 @@ angular.module("EmmetBlue")
 
 .controller("humanResourcesAccessControlController", function($scope, utils){
 	$scope.departments = {};
+	$scope.resources = {};
 	$scope.departmentGroups = [];
+
+	$scope.department = {};
 
 	var loadDepartments = function(){
 		var loadDepartments = utils.serverRequest('/human-resources/department/view', 'GET');
@@ -22,5 +25,52 @@ angular.module("EmmetBlue")
 			utils.errorHandler(responseObject);
 		})
 	}
+
+	var loadResources = function(role){
+		var department = $scope.departmentSelector;
+		var loadResources = utils.serverRequest('/permission/access-control/view-permissions?resourceId=0&role='+role+'&department='+department, 'GET');
+
+		loadResources.then(function(response){
+			$scope.resources = response;
+		}, function(responseObject){
+			utils.errorHandler(responseObject);
+		})
+	}
+
+	$scope.loadDepartment = function(department, name){
+		$scope.departmentSelector = name;
+		var rolePromise = utils.serverRequest('/human-resources/role/view-by-department?resourceId='+department, 'GET');
+
+		rolePromise.then(function(response){
+			$scope.department.role = response;
+		}, function(response){
+			utils.errorHandler(response);
+		})
+
+	}
+
+	$scope.$watch("roleSelector", function(newValue, oldValue){
+		if (typeof newValue !== "undefined"){
+			loadResources(newValue);
+		}
+	})
+
+	$scope.changePermission = function(resource, department, permission, status){
+		var permissionChangeRequest = utils.serverRequest("/permission/access-control/set-permission", "POST", {
+			"department":$scope.departmentSelector,
+			"role": $scope.roleSelector,
+			"resource-name":resource,
+			"permission-department":department,
+			"status":status,
+			"permission":permission
+		});
+
+		permissionChangeRequest.then(function(response){
+			utils.alert("Changes Saved", "ACL Updated successfully", "success", "notify");
+		}, function(response){
+			utils.errorHandler(response);
+		})
+	}
+
 	loadDepartments();
 })
