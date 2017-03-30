f100f569be9147bcc2bcc4259ec76e0ce5fd0d56
samshal <samueladeshina73@gmail.com>
2017-03-30 09:30:08 +0100

    diagnosis

--- plugins/consultancy/assets/controllers/lab-request-form-directive.js
+++ plugins/consultancy/assets/controllers/lab-request-form-directive.js
@@ -8,6 +8,37 @@ angular.module("EmmetBlue")
 		},
 		templateUrl: "plugins/consultancy/assets/includes/lab-request-form-template.html",
 		controller: function($scope, utils){
+			var table = $(".lab-table tr");
+
+			$("#lab-form > div > .table-responsive").css("border", "1px solid #000");
+
+			table.each(function(){
+
+				var th = $(this).children("th").first();
+				var td = $(this).children("td").first();
+
+				th.html("<div class='checkbox'><label><input type='checkbox' class='control-primary'/> "+th.html()+"</label></div>").addClass("bg-danger")
+				td.html("<div class='checkbox checkbox-right'><label><input type='checkbox' class='control-primary'/> "+td.html()+"</label></div>")
+			});
+
+			$scope.submit = function(){
+				var form = $("#lab-form").prop("outerHTML");
+				console.log(form);
+				form = btoa(encodeURIComponent(form).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+			        return String.fromCharCode('0x' + p1);
+			    }));
+				var data = {
+					patientID: 7,
+					clinicalDiagnosis: form,
+					requestedBy: utils.userSession.getID()
+				}
+
+				utils.serverRequest('/lab/lab-request/new', 'POST', data).then(function(response){
+					utils.notify("Operation Successful", "Request sent successfully", "success");
+				}, function(error){
+					utils.errorHandler(error);
+				})
+			}
 		}
 	}
 })
