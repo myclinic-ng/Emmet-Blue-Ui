75dc47e76fc04dd313c1d4b250fe208ba4f44ee4
samshal <samueladeshina73@gmail.com>
2017-04-13 10:13:33 +0100

    Seminal investigation added to lab forms, pharmacy dispensation log now persistent

--- plugins/pharmacy/assets/controllers/dispensation-controller.js
+++ plugins/pharmacy/assets/controllers/dispensation-controller.js
@@ -1,6 +1,7 @@
 angular.module("EmmetBlue")
 
 .controller('pharmacyDispensationController', function($scope, utils, patientEventLogger, $rootScope){
+	$scope.loadImage = utils.loadImage;
 	$scope.patient = {};
 
 	$scope.$on("loadPatientNumberForDispensation", function(){
@@ -39,17 +40,51 @@ angular.module("EmmetBlue")
 		var buttons = "<div class='btn-group'>"+viewButton+"</button>";
 		return buttons;
 	}
+
+	$scope.requestFilter = {
+		type: 'status',
+		description: 'Open Acknowledged Requests',
+		value: -1
+	}
+
+	$("option[status='disabled']").attr("disabled", "disabled");
 	
 	$scope.dtInstance = {};
 	$scope.dtOptions = utils.DT.optionsBuilder
 	.newOptions()
 	.withFnServerData(function(source, data, callback, settings){
+		var filter = $scope.requestFilter;
+		var _filter = "";
+		if (filter.type == 'date'){
+			var dates = filter.value.split(" - ");
+			_filter += 'filtertype=date&startdate='+dates[0]+'&enddate='+dates[1];
+		}
+		else if (filter.type == 'status'){
+			_filter += 'filtertype=status&query='+filter.value;
+		}
+		else if (filter.type == 'patient'){
+			_filter += 'filtertype=patient&query='+filter.value;
+		}
+		else if (filter.type == 'department'){
+			_filter += 'filtertype=department&query='+filter.value;
+		}
+		else if (filter.type == 'staff'){
+			_filter += 'filtertype=staff&query='+filter.staff;
+			_filter += "&_date="+filter.date;
+
+		}
+		else if (filter.type == 'patienttype'){
+			_filter += 'filtertype=patienttype&query='+filter.value;
+		}
+
+		$scope.currentRequestsFilter = _filter;
 		var draw = data[0].value;
         var order = data[2].value;
         var start = data[3].value;
         var length = data[4].value;
 
-        var url = '/pharmacy/dispensation/view?resourceId=0&paginate&from='+start+'&size='+length;
+        var url = '/pharmacy/dispensation/view?'+_filter+'&resourceId=0&paginate&from='+start+'&size='+length;
+
 		if (typeof data[5] !== "undefined" && data[5].value.value != ""){
 			url += "&keywordsearch="+data[5].value.value;
 		}
@@ -74,7 +109,6 @@ angular.module("EmmetBlue")
 	.withOption('paging', true)
 	.withPaginationType('full_numbers')
 	.withDisplayLength(10)
-	.withFixedHeader()
 	.withOption('createdRow', function(row, data, dataIndex){
 		utils.compile(angular.element(row).contents())($scope);
 	})
@@ -117,8 +151,39 @@ angular.module("EmmetBlue")
 	]);	
 
 	$scope.dtColumns = [
-		utils.DT.columnBuilder.newColumn(null).withTitle("Patient").renderWith(function(data){
-			return "<span class='text-bold'>"+data.PatientFullName+"</span> <br/>"+data.PatientUUID+"";
+		utils.DT.columnBuilder.newColumn(null).withTitle("").notSortable().renderWith(function(data, full, meta){
+			switch(data.Acknowledged){
+				case "1":{
+					var html = "<i class='icon-checkmark3 text-success'></i>";
+					break;
+				}
+				case "-1":{
+					var html = "<i class='fa fa-pause text-danger-400'></i>";
+					break;
+				}
+				default:{
+					var html = "<span class='text-grey-400 fa fa-dot-circle-o'></span>";
+				}
+			}
+			
+			return html;
+		}),
+		utils.DT.columnBuilder.newColumn(null).withTitle("Patient").renderWith(function(data, full, meta){
+			var image = $scope.loadImage(data.PatientPicture);
+			var html = "<td>"+
+							"<div class='media-left media-middle'>"+
+								"<a href='#'><img src='"+image+"' class='img-circle img-xs' alt=''></a>"+
+							"</div>"+
+							"<div class='media-left'>"+
+								"<div class=''><a href='#' class='text-default text-bold'>"+data.PatientFullName+"</a></div>"+
+								"<div class='text-muted text-size-small'>"+
+									"<span class='status-mark border-blue position-left'></span>"+
+									data.PatientUUID+
+								"</div>"+
+							"</div>"+
+						"</td>";
+
+			return html;
 		}),
 		utils.DT.columnBuilder.newColumn(null).withTitle("Dispensory").renderWith(function(data){
 			return "<span class='text-bold'>"+data.Dispensory+"</span> <br/>"+data.StoreName+"";
@@ -130,7 +195,7 @@ angular.module("EmmetBlue")
 	];
 
 	$scope.reloadDispensationsTable = function(){
-		$scope.dtInstance.reloadData();
+		$scope.dtInstance.rerender();
 	}
 
 	function loadDispensories(){
@@ -150,6 +215,21 @@ angular.module("EmmetBlue")
 			utils.errorHandler(error);
 		})
 	}
+
+	$scope.patientTypes = {};
+
+	$scope.loadPatientTypes = function(){
+		if (typeof (utils.userSession.getID()) !== "undefined"){
+			var requestData = utils.serverRequest("/patients/patient-type-category/view", "GET");
+			requestData.then(function(response){
+				$scope.patientTypes = response;
+			}, function(responseObject){
+				utils.errorHandler(responseObject);
+			});
+		}
+	}
+
+	$scope.loadPatientTypes();
 
 	$scope.currentDispensory;
 	loadDispensories();
@@ -404,6 +484,101 @@ angular.module("EmmetBlue")
 
 				$("#ack_view_modal").modal("show");
 				break;
+			}
+		}
+	}
+
+	$scope.getDateRange = function(selector){
+		var today = new Date();
+		switch(selector){
+			case "today":{
+				return today.toLocaleDateString() + " - " + today.toLocaleDateString();
+			}
+			case "yesterday":{
+				var yesterday = new Date(new Date(new Date()).setDate(new Date().getDate() - 1)).toLocaleDateString();
+				return yesterday + " - " + yesterday;
+				break;
+			}
+			case "week":{
+				var d = new Date(today);
+			  	var day = d.getDay(), diff = d.getDate() - day + (day == 0 ? -6 : 1);
+
+			  	return new Date(d.setDate(diff)).toLocaleDateString() + " - " + today.toLocaleDateString();
+			  	break;
+			}
+			case "month":{
+				var year = today.getFullYear();
+				var month = today.getMonth() + 1;
+
+				return month+'/1/'+year + ' - ' + today.toLocaleDateString();
+				break;
+			}
+		}
+	}
+
+	$scope.activateFilter = function(){
+		var selector = $scope.filterSelector;
+		switch(selector.type){
+			case "status":{
+				if (selector.value !== null){
+				$scope.requestFilter.type = "status";
+					var value = selector.value.split("<seprator>");
+					$scope.requestFilter.value = value[1];
+					$scope.requestFilter.description = "Status: "+value[0];
+					$scope.reloadDispensationsTable();
+				}
+				break;
+			}
+			case "dateRange":{
+				$scope.requestFilter.type = "date";
+				var value = selector.value.split(" - ");
+				$scope.requestFilter.value= selector.value;
+				$scope.requestFilter.description = "Date Ranges Between: "+ new Date(value[0]).toDateString()+" And "+ new Date(value[1]).toDateString();
+				$scope.reloadDispensationsTable();
+				break;
+			}
+			case "patient":{
+				$scope.requestFilter.type = "patient";
+				$scope.requestFilter.value= selector.value;
+				$scope.requestFilter.description = "Patient Search: '"+selector.value+"'";
+				$scope.reloadDispensationsTable();
+				break;
+			}
+			case "staff":{
+				$scope.requestFilter.type = "staff";
+				$scope.requestFilter.value= selector.value;
+				if (typeof selector.date !== "undefined" && selector.date !== ""){
+					$scope.requestFilter.date= selector.date;	
+				}
+				else {
+					$scope.requestFilter.date = (new Date()).toLocaleDateString();
+				}
+
+				$scope.requestFilter.description = "Staff Search: '"+selector.value+"'";
+
+				var req = utils.serverRequest("/human-resources/staff/get-staff-id?name="+selector.value, "GET");
+				req.then(function(result){
+					if (typeof result["StaffID"] !== "undefined"){
+						$scope.requestFilter.staff = result["StaffID"];
+						$scope.reloadDispensationsTable();
+					}
+				})
+				break;
+			}
+			case "patienttype":{
+				$scope.requestFilter.type = "patienttype";
+				var value = selector.value.split("<seprator>");
+				$scope.requestFilter.value = value[1];
+				$scope.requestFilter.description = "Patient Type: '"+value[0]+"'";
+				$scope.reloadDispensationsTable();
+				break;
+			}
+			default:{
+				$scope.requestFilter.type = "date";
+				var value = selector.type.split("<seprator>");
+				$scope.requestFilter.value = value[1];
+				$scope.requestFilter.description = value[0];
+				$scope.reloadDispensationsTable();
 			}
 		}
 	}
