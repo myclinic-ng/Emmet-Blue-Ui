3f0e0cd3150e0cef966d2ddcfd1f233e52483022
samshal <samueladeshina73@gmail.com>
2018-04-19 03:59:09 +0100

    Fixed setup and converted it to a global setup

--- plugins/user/assets/controllers/setup-controller.js
+++ plugins/user/assets/controllers/setup-controller.js
@@ -16,13 +16,15 @@ angular.module("EmmetBlue")
 
 			switch(businessType){
 				case "pharmacy":{
-					bootstrapPharmacy();
+					bootstrap("pharmacy");
 					break;
 				}
 				case "lab":{
+					bootstrap("lab");
 					break;
 				}
 				case "hospital":{
+					bootstrap("hospital");
 					break;
 				}
 				default:{
@@ -36,64 +38,20 @@ angular.module("EmmetBlue")
 		}
 	}
 
-	var bootstrapPharmacy = function(){
-		var req = _requests.createDepartment('Pharmacy Quick Setup');
+	var bootstrap = function(profile){
+		var prefRole = "general";
+		if (profile == "pharmacy"){
+			prefRole = "standalone";
+		}
+
+		var req = utils.serverRequest("/setup/init/run?profile="+profile+"&pref-role="+prefRole, "GET");
 		req.then(function(response){
-			$scope.deptId = response.lastInsertId;
-			$scope.redirectMessage = "Creating a secure tunnel ...";
-			_requests.createDepartmentUrl($scope.deptId, 'user/pharmacy-setup/index')
-			.then(function(response){
-				$scope.redirectMessage = "Enabling access to bootstrapped tunnel ...";
-				_requests.assignSecondaryDepartment($scope.deptId, utils.userSession.getID())
-				.then(function(response){
-					_requests.switchDepartment($scope.deptId, utils.userSession.getID())
-					.then(function(response){
-						$scope.redirectMessage = "Redirecting to set-up page ...";
-						updateCookieDashboardUrl(response.Url, response.DepartmentID);
-					}, function(error){ utils.errorHandler(error) });
-				}, function(error){ utils.errorHandler(error) });
-			}, function(error){ utils.errorHandler(error) });
+			$scope.redirectMessage = "Redirecting to account activation page...";
+			utils.notify("Default Accounts Created & Initialized Successfully", "Congratulations, Your EmmetBlue instance has been is ready for use", "success");
+
+			window.location.assign('user/activate-profile');
 		}, function(error){
 			utils.errorHandler(error);
 		})
-	}
-
-	var _requests = {
-		createDepartment: function(name){
-			return utils.serverRequest("/human-resources/department/new", "POST", {
-				name: name,
-				groupId: 1
-			});
-		},
-		createDepartmentUrl: function(id, url){
-			return utils.serverRequest("/human-resources/department/new-root-url", "POST", {
-				url: url,
-				department: id
-			});
-		},
-		assignSecondaryDepartment: function(dept, staff){
-			return utils.serverRequest("/human-resources/staff-department/assign-secondary", "POST", {
-				staff: staff,
-				department: dept
-			});
-		},
-		switchDepartment: function(dept, staff){
-			return utils.serverRequest("/user/account/get-switch-data", "POST", {
-				staff: staff,
-				department: dept
-			});
-		}
-	}
-
-
-	function updateCookieDashboardUrl(url, department = ""){
-		var cookie = $cookies.getObject(utils.globalConstants.USER_COOKIE_IDENTIFIER);
-		cookie.dashboard = url;
-		$cookies.putObject(utils.globalConstants.USER_COOKIE_IDENTIFIER, cookie);
-		
-		$location.path(url);
-		if (department !== ""){
-			utils.storage.currentStaffDepartmentID = department
-		}
 	}
 })
