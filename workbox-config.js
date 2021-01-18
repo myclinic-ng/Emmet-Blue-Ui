d3b61cc7af66f8fc1409361427e098f5bb59be58
samshal <samueladeshina73@gmail.com>
2021-01-18 17:25:33 +0100

    Installed lsw for spa serving

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "app/",
+  "globPatterns": [
+    "**/*.{js,json,html,css,eot,svg,ttf,woff,woff2,otf,png,xml,ico,webmanifest,jpg,zip,db,md,swf,xap,sh,as,txt,fla}"
+  ],
+  "swDest": "app/sw.js"
+};
