d790ff80ec81a08c4698eb717eea1d677960bf36
samshal <samueladeshina73@gmail.com>
2021-03-15 17:46:18 +0100

    new changes

--- plugins/user/assets/controllers/login-controller.js
+++ plugins/user/assets/controllers/login-controller.js
@@ -104,7 +104,7 @@ angular.module("EmmetBlue")
 
 					if (typeof response.business == "undefined"){
 						utils.serverRequest("/read-resource?url=business-info", "GET").then(function(resource){
-							responseObject.businessInfo = $.parseJSON(resource);
+							responseObject.businessInfo = $.parseJSON(JSON.Stringify(resource));
 							$cookies.putObject(utils.globalConstants.USER_COOKIE_IDENTIFIER, responseObject);
 						}, function(error){
 
