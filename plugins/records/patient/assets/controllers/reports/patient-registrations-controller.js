f95ddc29201902cb370d7485db10ad0bb82d1975
samshal <samueladeshina73@gmail.com>
2021-04-12 17:25:54 +0100

    patient visit updates

--- plugins/records/patient/assets/controllers/reports/patient-registrations-controller.js
+++ plugins/records/patient/assets/controllers/reports/patient-registrations-controller.js
@@ -9,10 +9,18 @@ angular.module("EmmetBlue")
 				$scope.pageSegment = urlPart+"patient-registrations-report.html";
 				break;
 			}
+			case "patient-visits":{
+				$scope.pageSegment = urlPart+"patient-visits-report.html";
+				break;
+			}
+			default:{
+				$scope.pageSegment = urlPart+"ward-admissions-report.html";
+				break;
+			}
 		}
 	}
 
-	$scope.loadPageSegment('new-registration');
+	$scope.loadPageSegment('patient-visits');
 
 	$scope.dates = [];
 	$scope.$watch(function(){
@@ -22,10 +30,22 @@ angular.module("EmmetBlue")
 		reloadData();
 	});
 
+	$scope.report = {
+		visits: {},
+		registrations: {}
+	};
+
 	function reloadData(){
 		var req = utils.serverRequest("/patients/reports/get-total-registration?startdate="+$scope.dates[0]+"&enddate="+$scope.dates[1], "GET");
 		req.then(function(response){
-			$scope.report = response;
+			$scope.report.registrations = response;
+		}, function(error){
+			utils.errorHandler(error);
+		})
+
+		var req = utils.serverRequest("/patients/reports/get-total-visits?startdate="+$scope.dates[0]+"&enddate="+$scope.dates[1], "GET");
+		req.then(function(response){
+			$scope.report.visits = response;
 		}, function(error){
 			utils.errorHandler(error);
 		})
@@ -56,6 +76,38 @@ angular.module("EmmetBlue")
 		};
 
 		var req = utils.serverRequest("/patients/reports/get-total-registration-by-category?startdate="+$scope.dates[0]+"&enddate="+$scope.dates[1]+"&category="+category, "GET");
+		req.then(function(response){
+			$scope.currentCategory.types = response;
+		}, function(error){
+			utils.errorHandler(error);
+		})
+	}
+})
+
+.controller("recordsReportsPatientVisitsTabRegController", function($scope, utils){
+	$scope.dates = [];
+	$scope.$watch(function(){
+		return utils.storage.currentReportDateRange;
+	}, function(value){
+		$scope.dates = value;
+		reloadData();
+	});
+
+	function reloadData(){
+		var req = utils.serverRequest("/patients/reports/get-total-visits-by-categories?startdate="+$scope.dates[0]+"&enddate="+$scope.dates[1], "GET");
+		req.then(function(response){
+			$scope.categoriesReport = response;
+		}, function(error){
+			utils.errorHandler(error);
+		})
+	}
+
+	$scope.loadCategory = function(category){
+		$scope.currentCategory = {
+			id: category
+		};
+
+		var req = utils.serverRequest("/patients/reports/get-total-visits-by-category?startdate="+$scope.dates[0]+"&enddate="+$scope.dates[1]+"&category="+category, "GET");
 		req.then(function(response){
 			$scope.currentCategory.types = response;
 		}, function(error){
