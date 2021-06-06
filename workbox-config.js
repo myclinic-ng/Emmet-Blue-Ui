fd87547d0dba41037f2fa99c38144923cbcd0993
samshal <samueladeshina73@gmail.com>
2021-06-06 13:13:29 +0100

    regenerated service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	globDirectory: 'app/',
+	globDirectory: 'C:/dev/projects/emmetblue/Emmet-Blue-Ui',
 	globPatterns: [
-		'**/*.{js,json,html,css,eot,svg,ttf,woff,woff2,otf,gif,png,xml,ico,webmanifest,jpg,zip,db,md,swf,xap,sh,as,txt,fla}'
+		'**/*.{js,json,css,html,eot,svg,ttf,woff,woff2,otf,gif,png,xml,ico,webmanifest,jpg,zip,db,md,swf,xap,sh,as,txt,fla}'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	],
-	swDest: 'app/sw.js'
+	swDest: 'C:/dev/projects/emmetblue/Emmet-Blue-Ui/sw.js'
 };
