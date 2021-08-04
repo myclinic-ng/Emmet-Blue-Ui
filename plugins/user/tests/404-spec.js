d50b9b03220ed366c7d6d8bc40944bcc5ee6f99a
samshal <samueladeshina73@gmail.com>
2021-08-04 09:51:21 +0100

    Adding tests

---
+++ plugins/user/tests/404-spec.js
@@ -0,0 +1,21 @@
+describe('emmetblue user/404', function(){
+	var appPath = "http://localhost:9898/";
+	var originalTimeout;
+
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = jasmine.DEFAULT_TIMEOUT_INTERVAL*10;
+    });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+    
+	it('should not allow anon users', function(){
+		browser.get(appPath+'user/404');
+
+		var url = browser.getCurrentUrl();
+		expect(url).toEqual(appPath+'user/login');
+	})
+})
+
